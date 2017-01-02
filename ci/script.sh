e6f0e1cb35187135fc061b86c86465250bde4b1e
Andrew Gallant <jamslam@gmail.com>
2017-01-02 14:19:42 -0500

    progress

--- ci/script.sh
+++ ci/script.sh
@@ -7,29 +7,29 @@ set -e
 # Builds the regex crate and runs tests.
 cargo build --verbose
 cargo doc --verbose
-if [ "$TRAVIS_RUST_VERSION" = "nightly" ]; then
-  cargo build --verbose --manifest-path regex-debug/Cargo.toml
-  RUSTFLAGS="-C target-feature=+ssse3" cargo test --verbose --features 'simd-accel pattern' --jobs 2
-else
-  cargo test --verbose --jobs 2
-fi
+#if [ "$TRAVIS_RUST_VERSION" = "nightly" ]; then
+#  cargo build --verbose --manifest-path regex-debug/Cargo.toml
+#  RUSTFLAGS="-C target-feature=+ssse3" cargo test --verbose --features 'simd-accel pattern' --jobs 2
+#else
+#  cargo test --verbose --jobs 2
+#fi
 
 # Run a test that confirms the shootout benchmarks are correct.
 ci/run-shootout-test
 
 # Run tests on regex-syntax crate.
-cargo test --verbose --manifest-path regex-syntax/Cargo.toml
+#cargo test --verbose --manifest-path regex-syntax/Cargo.toml
 cargo doc --verbose --manifest-path regex-syntax/Cargo.toml
 
 # Run tests on regex-capi crate.
 cargo build --verbose --manifest-path regex-capi/Cargo.toml
-(cd regex-capi/ctest && ./compile && LD_LIBRARY_PATH=../target/debug ./test)
-(cd regex-capi/examples && ./compile && LD_LIBRARY_PATH=../target/release ./iter)
+#(cd regex-capi/ctest && ./compile && LD_LIBRARY_PATH=../target/debug ./test)
+#(cd regex-capi/examples && ./compile && LD_LIBRARY_PATH=../target/release ./iter)
 
 # Make sure benchmarks compile. Don't run them though because they take a
 # very long time.
-if [ "$TRAVIS_RUST_VERSION" = "nightly" ]; then
-  for x in rust rust-bytes pcre1 onig; do
-    (cd bench && ./run $x --no-run)
-  done
-fi
+#if [ "$TRAVIS_RUST_VERSION" = "nightly" ]; then
+#  for x in rust rust-bytes pcre1 onig; do
+#    (cd bench && ./run $x --no-run)
+#  done
+#fi
